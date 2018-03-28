0ff16cc4473a1ea7745c5fae43fa4826dce6c148
Davis Shaver <davisshaver@gmail.com>
2018-03-28 14:11:40 -0400

    Use full image

--- wp-parsely.php
+++ wp-parsely.php
@@ -538,7 +538,7 @@ class Parsely {
 
 			if ( has_post_thumbnail() ) {
 				$image_id  = get_post_thumbnail_id();
-				$image_url = wp_get_attachment_image_src( $image_id );
+				$image_url = wp_get_attachment_image_src( $image_id, 'full' );
 				$image_url = $image_url[0];
 			} else {
 				$image_url = $this->get_first_image( $post );
